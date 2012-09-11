a4d98842c7574a6ce8c6df2c8a73a457c19391ed
Ichiro Yamamoto <yichi@otakukingdom.com>
2012-09-11 18:53:18 -0400

    schema change

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,18 @@
 class Url < ActiveRecord::Base
   attr_accessible :url
 
+  after_save :create_short_url
+
   validates :url, :presence => true
+
+  
+  # For simplicity we are using id
+  # as the short url, we can change this
+  # field to be a randomly generated string
+  def create_short_url
+    if self.short_url.nil?
+      self.short_url = self.id
+      self.save
+    end
+  end
 end
