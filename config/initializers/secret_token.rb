67aa1081966d4c5698957c1ec6bdeb32193a9d89
Ichiro Yamamoto <yichi@otakukingdom.com>
2012-09-11 18:16:09 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrlRedir::Application.config.secret_token = '95174554fce54bb23e28a790719cfc9f07028dd5341206629669f8e90eeb5fc7c375153bee7d7759a41f91b4ab54e09723aeb29f50305dd49d52519dd0198486'
